45399d62b31fad6e48fd50c088724c3d7cf996b5
lichq <lichq@yutong,com>
2024-10-22 17:14:47 +0800

    add Constructor to Worker class

--- character/src/main/java/com/dwind/character/pojo/Worker.java
+++ character/src/main/java/com/dwind/character/pojo/Worker.java
@@ -1,11 +1,14 @@
 package com.dwind.character.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Worker {
     public String name;
     public int age;
     public String gender;
+
 
 }
